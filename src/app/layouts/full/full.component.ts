b170c64b226b185a03e75000a1df8f6813787f54
rkoren1 <rok.koren@netica.si>
2024-05-14 18:39:33 +0200

    project init with free flexy theme

---
+++ src/app/layouts/full/full.component.ts
@@ -0,0 +1,38 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+interface sidebarMenu {
+  link: string;
+  icon: string;
+  menu: string;
+}
+
+@Component({
+  selector: 'app-full',
+  templateUrl: './full.component.html',
+  styleUrls: ['./full.component.scss'],
+})
+export class FullComponent {
+  search: boolean = false;
+
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  routerActive: string = 'activelink';
+
+  sidebarMenu: sidebarMenu[] = [
+    {
+      link: '/home',
+      icon: 'home',
+      menu: 'Dashboard',
+    },
+  ];
+}
